5e061f95569ddd63ba34dadaa77c5f482e198a0a
Anzu <anzunakayama@gmail.com>
2020-12-01 15:49:04 -0800

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyCpYRvUylEpllK_okfsvUFEX0nxtH89vvY",
+  authDomain: "ecommerce-demo-84728.firebaseapp.com",
+  databaseURL: "https://ecommerce-demo-84728.firebaseio.com",
+  projectId: "ecommerce-demo-84728",
+  storageBucket: "ecommerce-demo-84728.appspot.com",
+  messagingSenderId: "1041531026510",
+  appId: "1:1041531026510:web:3d123b2d724023705a971f",
+  measurementId: "G-9TX59XG0W0"
+};
---
+++ src/templates/SignUp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SignUp = () ={
+
+  return(
+    <div className="c-section-container">
+      <h2 className="u-text-center u-text__headline">Register account</h2>
+    </div>
+    
+  )
+}
+
+export default SignUp
